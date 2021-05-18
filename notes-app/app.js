f3c740e5bf0275e6eed1c416da799ec319313104
snachit36 <sbhadgal_be19@thapar.edu>
2021-05-18 20:03:20 +0530

    importing npm packages

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
+const validator = require("validator");
 const getNotes = require("./notes");
 
 message = getNotes();
-
 console.log(message);
+
+console.log(validator.isEmail("gmail.com"));
+console.log(validator.isURL("http://petarmy.in"));
