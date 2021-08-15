2e8ec9d9b71af24e62505e1f1f463ea95cc9e910
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-15 11:58:43 +0530

    Worked with yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,59 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes");
 
-message = getNotes();
-console.log(message);
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log(chalk.red.inverse.bold.underline("Error!"));
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+  command: "add",
+  description: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  description: "Remove a note",
+  handler: function () {
+    console.log("Removing a new note");
+  },
+});
+
+// Create list command
+yargs.command({
+  command: "list",
+  description: "List your notes",
+  handler: function () {
+    console.log("Listing out all  notes");
+  },
+});
+
+// Create read command
+yargs.command({
+  command: "read",
+  description: "Read a note",
+  handler: function () {
+    console.log("Reading a new note");
+  },
+});
+
+yargs.parse();
