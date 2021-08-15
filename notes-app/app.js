362f414a26d534e37013bbf6091832d2d283d78a
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-15 15:46:21 +0530

    Added functionality for adding and removing the notes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,6 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
-const getNotes = require("./notes");
+const notes = require("./notes");
 
 // Customize yargs version
 yargs.version("1.1.0");
@@ -24,8 +24,7 @@ yargs.command({
     },
   },
   handler: function (argv) {
-    console.log("Title: " + argv.title);
-    console.log("Body: " + argv.body);
+    notes.addNote(argv.title, argv.body);
   },
 });
 
@@ -33,8 +32,15 @@ yargs.command({
 yargs.command({
   command: "remove",
   description: "Remove a note",
-  handler: function () {
-    console.log("Removing a new note");
+  builder: {
+    title: {
+      describe: "Note Title which is to be removed",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    notes.removeNote(argv.title);
   },
 });
 
