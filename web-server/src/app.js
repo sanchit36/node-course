c45de266a65e636eb6078375b10ee67d8f842d23
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-18 01:46:00 +0530

    Used hbs partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // Setup handlerbars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -23,7 +26,11 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { title: "Help", message: "Hellppppppppppp" });
+  res.render("help", {
+    title: "Help",
+    message: "Hellppppppppppp",
+    name: "Sanchit Bhadgal",
+  });
 });
 
 app.get("/weather", (req, res) => {
