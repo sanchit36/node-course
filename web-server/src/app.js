c41fa0be41bc343f15c75b44d34bfa3822cbcd00
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-17 21:33:35 +0530

    Learned using handlerbars and setup static files with express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,36 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Hello express!");
-});
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
-app.get("/help", (req, res) => {
-  res.send("Help page");
+// Setup handlerbars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", { title: "Weather App", name: "Sanchit Bhadgal" });
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.render("about", { title: "About", name: "Sanchit Bhadgal" });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", { title: "Help", message: "Hellppppppppppp" });
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.send({
+    forecast: "Its 30 degrees",
+    location: "Delhi",
+  });
 });
 
 app.listen(3000, () => {
