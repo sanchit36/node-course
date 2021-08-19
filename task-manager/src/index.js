ec66f17a9bf63fd07c6598b6cc5ad3560bc0d9df
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-19 16:20:35 +0530

    Chages

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
-const User = require("./models/User");
-const Task = require("./models/Task");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,81 +9,9 @@ const PORT = process.env.PORT || 3000;
 // Express config
 app.use(express.json());
 
-app.post("/users", (req, res) => {
-  const user = new User(req.body);
-
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send(users);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
-
-app.post("/tasks", (req, res) => {
-  const task = new Task(req.body);
-
-  task
-    .save()
-    .then(() => {
-      res.status(201).send(task);
-    })
-    .catch((error) => {
-      res.status(400).send(error);
-    });
-});
-
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-});
+// Routes
+app.use(userRouter);
+app.use(taskRouter);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const Task = require("../models/Task");
+const router = new express.Router();
+
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
+
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  const updates = Object.keys(req.body);
+  const allowedUpdate = ["description", "completed"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdate.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(_id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send({ error: "Task not found" });
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(_id);
+    if (!task) {
+      return res.status(404).send({ error: "Task not found" });
+    }
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
