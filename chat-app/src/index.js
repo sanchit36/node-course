538aa8889b2a6c6a33afd1c963e193b6e26347eb
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-21 23:15:47 +0530

    chat app init

---
+++ chat-app/src/index.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection", () => {
+  console.log("New webSocket connection");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}!`);
+});
