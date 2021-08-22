7493614eaad20ccfcbbcc6341e89ea7d19e8344a
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-22 21:41:04 +0530

    Added timeStamp

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -15,14 +15,16 @@ const locationUrlTemplate = document.getElementById(
 
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
-    message,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
 
-socket.on("locationMessage", (url) => {
+socket.on("locationMessage", (message) => {
   const html = Mustache.render(locationUrlTemplate, {
-    url,
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
