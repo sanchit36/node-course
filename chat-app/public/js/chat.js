5220c20aab0e9726b899eac8d511041cf15f3657
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-22 20:52:51 +0530

    Acknowledgement in sockets

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -12,6 +12,31 @@ formElement.addEventListener("submit", (event) => {
   const message = inputElement.value;
   if (!message) return;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message has been delivered!");
+  });
+
   inputElement.value = "";
+});
+
+document.getElementById("send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser.");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location shared!");
+      }
+    );
+  });
 });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,9 +17,29 @@ io.on("connection", (socket) => {
   console.log("New webSocket connection");
 
   socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
 
-  socket.on("sendMessage", (message) => {
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+
     io.emit("message", message);
+    callback();
+  });
+
+  socket.on("sendLocation", (location, callback) => {
+    io.emit(
+      "message",
+      `https://google.com/maps?q=${location.latitude},${location.longitude}`
+    );
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("message", "A user has left!");
   });
 });
 
